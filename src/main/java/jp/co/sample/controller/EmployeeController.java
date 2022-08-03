cb67c8414fadb357d1a5550bd104234d2a673746
ryoko-taki <ryoko.taki@rakus-partners.co.jp>
2022-08-03 15:48:15 +0900

    従業員一覧の出力、Listの取得機能を追加

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -1,5 +1,23 @@
 package jp.co.sample.controller;
 
-public class EmployeeController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.service.EmployeeService;
+
+//従業員情報を検索する
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+	@Autowired
+	private EmployeeService employeeService;
+	
+	@RequestMapping("/showList")
+	public String showList(Model model) {
+		employeeService.showList();
+		model.addAttribute("employeeList", employeeService.showList());
+		return "employee/list";
+	}
 }
